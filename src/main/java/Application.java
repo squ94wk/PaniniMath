ad0e44419606c8b8f8252d84eb71f4138877031c
squawk <squawk@gmx.de>
2018-04-26 20:03:00 +0200

    Show Remaining time, accept program argument

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,11 @@
 public class Application {
   public static void main(String[] args) throws InterruptedException {
-    StatisticalTestCollector test = new StatisticalTestCollector(1000000);
+    int runs = 10000;
+    if (args.length == 1 && args[0].matches("\\d{1,8}")) {
+      runs = Integer.parseInt(args[0]);
+    }
+
+    StatisticalTestCollector test = new StatisticalTestCollector(runs);
 
     test.runTest();
     Thread.sleep(100);
--- src/main/java/ProgressBar.java
+++ src/main/java/ProgressBar.java
@@ -2,6 +2,8 @@ import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class ProgressBar extends Thread {
   private final int sleepInterval;
   private final long timeAtStart;
@@ -9,9 +11,10 @@ public class ProgressBar extends Thread {
   private final ProgressMeasurable process;
 
   public ProgressBar(final ProgressMeasurable process, final int sleepInterval) {
-
+    checkArgument(process != null, "Process must not be null.");
     this.process = process;
     this.progressWhenFinished = process.getProgressWhenFinished();
+    checkArgument(progressWhenFinished > 0, "progressWhenFinished must be greater than 0.");
     this.sleepInterval = sleepInterval;
     timeAtStart = System.currentTimeMillis();
   }
@@ -28,7 +31,8 @@ public class ProgressBar extends Thread {
         next = ' ';
       }
     }
-    builder.append(String.format("| %.1f%%  time: %s", ((double) process.getProgress() * 100) / progressWhenFinished, getTimeElapsed()));
+    final double percent = ((double) process.getProgress() * 100) / progressWhenFinished;
+    builder.append(String.format("| %.1f%%  time: %s (%s remaining)", percent, getTimeElapsed(), getTimeRemaining()));
     return builder.toString();
   }
 
@@ -47,17 +51,32 @@ public class ProgressBar extends Thread {
     System.out.println(String.format("\rdone. Time elapsed: %s.", getTimeElapsed()));
   }
 
-  private String getTimeElapsed() {
+  private String formatTime(long milliseconds) {
     final StringBuilder format = new StringBuilder();
-    long elapsedMilliseconds = System.currentTimeMillis() - timeAtStart;
-    if (elapsedMilliseconds > 1000 * 60) {
+    if (milliseconds > 1000 * 60) {
       format.append("%1$dm ");
     }
-    if (elapsedMilliseconds > 1000) {
+    if (milliseconds > 1000) {
       format.append("%2$ds ");
     }
-    final long seconds = (elapsedMilliseconds / 1000) % 60;
-    final long minutes = elapsedMilliseconds / 60 / 1000;
+    final long seconds = (milliseconds / 1000) % 60;
+    final long minutes = milliseconds / 60 / 1000;
     return String.format(format.toString(), minutes, seconds);
+  }
+
+  private String getTimeElapsed() {
+    long elapsedMilliseconds = System.currentTimeMillis() - timeAtStart;
+    return formatTime(elapsedMilliseconds);
+  }
+
+  private String getTimeRemaining() {
+    int progress = process.getProgress();
+    if (progress == 0) {
+      return "";
+    }
+    long elapsedMilliseconds = System.currentTimeMillis() - timeAtStart;
+    double timesRemaining = progressWhenFinished / ((double) progress) - 1;
+
+    return formatTime((int) Math.round(timesRemaining * elapsedMilliseconds));
   }
 }
